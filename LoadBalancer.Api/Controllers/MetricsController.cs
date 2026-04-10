f8f7794a298d2642bd7cbbf21ef742d45f6047b6
Gabriel Saturi <gabrielsaturi11@gmail.com>
2026-04-10 15:34:18 -0300

    git commit -m "feat(metrics): expose load balancer metrics endpoint"

---
+++ LoadBalancer.Api/Controllers/MetricsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LoadBalancer.Api.Services;
+
+namespace LoadBalancer.Api.Controllers
+{
+   
+    [ApiController]
+    [Route("metrics")]
+    public class MetricsController : ControllerBase
+    {
+        private readonly LoadBalancerService _lb;
+
+        public MetricsController(LoadBalancerService lb)
+        {
+            _lb = lb;
+        }
+
+        [HttpGet]
+        public IActionResult GetMetrics()
+        {
+            var data = _lb.GetAllInstances().Select(i => new
+            {
+                url = i.Url,
+                activeConnections = i.ActiveConnections,
+                totalRequests = i.TotalRequests,
+                failedRequests = i.FailedRequests,
+                avgResponseTime = i.AverageResponseTime
+            });
+
+            return Ok(data);
+        }
+    }
+}
