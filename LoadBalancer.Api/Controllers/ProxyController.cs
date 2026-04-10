15d6befb20696f9bbfc4563b222df32f3ef3849b
Gabriel Saturi <gabrielsaturi11@gmail.com>
2026-04-10 13:38:03 -0300

    git commit -m "feat: implement proxy controller to forward requests to backend instances"

---
+++ LoadBalancer.Api/Controllers/ProxyController.cs
@@ -0,0 +1,31 @@
+﻿namespace LoadBalancer.Api.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using LoadBalancer.Api.Services;
+
+    [ApiController]
+    [Route("{**catch-all}")]
+    public class ProxyController : ControllerBase
+    {
+        private readonly LoadBalancerService _lb;
+        private readonly HttpClient _httpClient;
+
+        public ProxyController(LoadBalancerService lb, IHttpClientFactory factory)
+        {
+            _lb = lb;
+            _httpClient = factory.CreateClient();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Forward()
+        {
+            var instance = _lb.GetNextInstance();
+
+            var response = await _httpClient.GetAsync($"{instance.Url}/ping");
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            return Content(content, "application/json");
+        }
+    }
+}
