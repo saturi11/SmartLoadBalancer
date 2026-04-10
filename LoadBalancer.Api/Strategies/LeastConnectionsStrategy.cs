dfedcd1915c4bba3009b570e6e237029afc23482
Gabriel Saturi <gabrielsaturi11@gmail.com>
2026-04-10 14:19:11 -0300

    git commit -m "feat(strategy): add least connections load balancing strategy"

---
+++ LoadBalancer.Api/Strategies/LeastConnectionsStrategy.cs
@@ -0,0 +1,15 @@
+﻿using LoadBalancer.Api.Models;
+namespace LoadBalancer.Api.Strategies
+{
+
+    public class LeastConnectionsStrategy : ILoadBalancingStrategy
+    {
+        public BackendInstance SelectInstance(List<BackendInstance> instances)
+        {
+            return instances
+                .OrderBy(i => i.ActiveConnections)
+                .ThenBy(_ => Guid.NewGuid())
+                .First();
+        }
+    }
+}
