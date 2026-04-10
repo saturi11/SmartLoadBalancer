e14e2e9bd7884f3ee6b2c03e466264b7746cb04a
Gabriel Saturi <gabrielsaturi11@gmail.com>
2026-04-10 15:33:54 -0300

    git commit -m "feat(metrics): add request, failure and response time tracking to backend instances"

--- LoadBalancer.Api/Models/BackendInstance.cs
+++ LoadBalancer.Api/Models/BackendInstance.cs
@@ -5,10 +5,28 @@
         public string Url { get; set; } = default!;
         public bool IsHealthy { get; set; } = true;
         private int _activeConnections;
+        private int _totalRequests;
+        private int _failedRequests;
+        private long _totalResponseTime;
 
         public int ActiveConnections => _activeConnections;
+        public int TotalRequests => _totalRequests;
+        public int FailedRequests => _failedRequests;
+        public long TotalResponseTime => _totalResponseTime;
 
         public void Increment() => Interlocked.Increment(ref _activeConnections);
         public void Decrement() => Interlocked.Decrement(ref _activeConnections);
+        public void IncrementConnections() => Interlocked.Increment(ref _activeConnections);
+        public void DecrementConnections() => Interlocked.Decrement(ref _activeConnections);
+
+        public void IncrementRequests() => Interlocked.Increment(ref _totalRequests);
+        public void IncrementFailures() => Interlocked.Increment(ref _failedRequests);
+
+        public void AddResponseTime(long ms) => Interlocked.Add(ref _totalResponseTime, ms);
+
+        public double AverageResponseTime =>
+            _totalRequests == 0 ? 0 : (double)_totalResponseTime / _totalRequests;
+
+
     }
 }
