54634a9e776c921caf056c5b2c6b462b681ec53a
Gabriel Saturi <gabrielsaturi11@gmail.com>
2026-04-10 13:37:27 -0300

    git commit -m "feat: introduce load balancing strategy interface"

---
+++ LoadBalancer.Api/Strategies/ILoadBalancingStrategy.cs
@@ -0,0 +1,9 @@
+﻿using LoadBalancer.Api.Models;
+
+namespace LoadBalancer.Api.Strategies
+{
+    public interface ILoadBalancingStrategy
+    {
+        BackendInstance SelectInstance(List<BackendInstance> instances);
+    }
+}
