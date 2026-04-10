cb894de252bcf78a73e10601f2f887e93d8f2b3d
Gabriel Saturi <gabrielsaturi11@gmail.com>
2026-04-10 13:53:05 -0300

    git commit -m "feat(health): implement background health check service to track backend availability and enable failover"

---
+++ LoadBalancer.Api/Services/HealthCheckService.cs
@@ -0,0 +1,40 @@
+﻿namespace LoadBalancer.Api.Services
+{
+    using LoadBalancer.Api.Models;
+
+    public class HealthCheckService : BackgroundService
+    {
+        private readonly LoadBalancerService _lb;
+        private readonly HttpClient _httpClient;
+
+        public HealthCheckService(LoadBalancerService lb, IHttpClientFactory factory)
+        {
+            _lb = lb;
+            _httpClient = factory.CreateClient();
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var instances = _lb.GetAllInstances();
+
+                foreach (var instance in instances)
+                {
+                    try
+                    {
+                        var response = await _httpClient.GetAsync($"{instance.Url}/health", stoppingToken);
+
+                        instance.IsHealthy = response.IsSuccessStatusCode;
+                    }
+                    catch
+                    {
+                        instance.IsHealthy = false;
+                    }
+                }
+
+                await Task.Delay(3000, stoppingToken); // roda a cada 3s
+            }
+        }
+    }
+}
