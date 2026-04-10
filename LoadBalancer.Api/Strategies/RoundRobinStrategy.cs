5ec4d20658ad904e351fbf4c9d76a1488f14a2c5
Gabriel Saturi <gabrielsaturi11@gmail.com>
2026-04-10 13:37:37 -0300

    git commit -m "feat: implement round robin load balancing strategy"

---
+++ LoadBalancer.Api/Strategies/RoundRobinStrategy.cs
@@ -0,0 +1,20 @@
+﻿namespace LoadBalancer.Api.Strategies
+{
+    using LoadBalancer.Api.Models;
+
+    public class RoundRobinStrategy : ILoadBalancingStrategy
+    {
+        private int _index = 0;
+        private readonly object _lock = new();
+
+        public BackendInstance SelectInstance(List<BackendInstance> instances)
+        {
+            lock (_lock)
+            {
+                var instance = instances[_index % instances.Count];
+                _index++;
+                return instance;
+            }
+        }
+    }
+}
