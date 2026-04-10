624755229cc895e7edb2981d403ff3cfca91246f
Gabriel Saturi <gabrielsaturi11@gmail.com>
2026-04-10 13:37:00 -0300

    git commit -m "feat: add BackendInstance model to represent backend nodes"

---
+++ LoadBalancer.Api/Models/BackendInstance.cs
@@ -0,0 +1,9 @@
+﻿namespace LoadBalancer.Api.Models
+{
+    public class BackendInstance
+    {
+        public string Url { get; set; } = default!;
+        public bool IsHealthy { get; set; } = true;
+        public int ActiveConnections { get; set; } = 0;
+    }
+}
